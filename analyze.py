a04d5ad223d7ea3b122e0d565a3efd3510940124
NolanR <ngr@uoregon.edu>
2020-03-02 23:44:26 -0800

    Functional nonblocking

--- analyze.py
+++ analyze.py
@@ -1,6 +1,9 @@
 import socket
 import logging
+import time
 import sys
+from multiprocessing import Process
+import os
 
 # Setup Logging
 logging.basicConfig(level = logging.DEBUG,
@@ -8,13 +11,16 @@ logging.basicConfig(level = logging.DEBUG,
                     datefmt="%Y-%m-%d_%H:%M:%S",
                     handlers=[logging.FileHandler("chat.log", encoding = "utf-8")])
 
+start_time = time.time()
+
 def main():
 
     # Argument checking
-    if len(sys.argv) != 2:
-        print("Usage  : $ python analyze <channel>")
-        print("Example: $ python analyze trainwreckstv")
+    if len(sys.argv) != 3:
+        print("Usage  : $ python analyze <channel> <duration>")
+        print("Example: $ python analyze trainwreckstv 100")
         print("Note   : You must have a file, privateToken.txt, given by https://twitchapps.com/tmi/")
+        print("Note   : The duration is given in seconds")
         return 1
 
     # Constants for data mining
@@ -30,14 +36,22 @@ def main():
     f.close()
 
     channel = sys.argv[1]
-    
-    listen(server, port, token, nickname, channel)
+    duration = sys.argv[2]
+    try:
+        duration = int(duration)
+    except ValueError:
+        print("Expected to receive an integer duration.")
+        return 1
+
+    listen(server, port, token, nickname, channel, duration)
 
     return 0
 
 # Establishes an IRC connection with Twitch
-def listen(server, port, pwd, nick, channel):
+def listen(server, port, pwd, nick, channel, dur):
     sock = socket.socket()
+    # We shouldn't be held up on receiving a message that doesn't come
+    sock.settimeout(0.5)
     sock.connect((server, port))
 
     # Authentication to server
@@ -45,18 +59,22 @@ def listen(server, port, pwd, nick, channel):
     sock.send(f"NICK {nick}\n".encode("utf-8"))
     sock.send(f"JOIN {channel}\n".encode("utf-8"))
 
-    while True:
-        resp = sock.recv(2048).decode("utf-8")
+    while time.time() - start_time < dur:
+        print("Time: ", time.time() - start_time)
+        try:
+            resp = sock.recv(2048).decode("utf-8")
+        except:
+            pass
 
         # Twitch will periodically send keyword PING to check for connection
         if resp.startswith("PING"):
-            logging.info("Got ping request")
             sock.send("PONG\n".encode("utf-8"))
         elif len(resp) > 0:
             logging.info(resp)
 
     sock.close()
 
-
 if __name__ == "__main__":
+    # Run main functionality
     main()
+
